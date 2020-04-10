967b37bae49d907f200d1a5a63a6974fd9f9bdf9
mildc055ee <uncxochtil@gmail.com>
2020-04-11 03:19:41 +0900

    impl lexer

---
+++ src/lexer.rs
@@ -0,0 +1,81 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum TokenKind {
+    LParen,
+    RParen,
+    Symbol,
+    Int,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Token {
+    kind: TokenKind,
+    val: String,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, val: String) -> Token {
+        Token{
+            kind: kind,
+            val: val,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    src: Peekable<Chars<'a>>,
+    token_list: Vec<Token>
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(src: &'a str) -> Lexer<'a> {
+        Lexer{
+            src: src.chars().peekable(),
+            token_list: vec![]
+        }
+    }
+
+    pub fn lex(&mut self) -> Result<&Vec<Token>, String> {
+        while let Some(c) = self.src.next() {
+            match c {
+                '(' => self.token_list.push(Token::new(TokenKind::LParen, "(".to_string())),
+                ')' => self.token_list.push(Token::new(TokenKind::RParen, ")".to_string())),
+                n if c.is_numeric() => {
+                    let mut num = String::new();
+                    num.push(n);
+                    while let Some(&i) = self.src.peek() {
+                        if i.is_numeric() {
+                            num.push(i);
+                            self.src.next();
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    self.token_list.push(Token::new(TokenKind::Int, num))
+                },
+                _w if c.is_whitespace() => continue,
+                s if c.is_ascii() => {
+                    let mut symbol = String::new();
+                    symbol.push(s);
+                    while let Some(&w) = self.src.peek() {
+                        if w.is_whitespace() {
+                            break;
+                        }
+                        else {
+                            symbol.push(w);
+                            self.src.next();
+                        }
+                    }
+                    self.token_list.push(Token::new(TokenKind::Symbol, symbol))
+                },
+                _ => return Err("invalid character detected.".to_string())
+            };
+        }
+
+        Ok(&self.token_list)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 pub mod data;
 pub mod func;
+pub mod lexer;
 //mod env;
+use lexer::Lexer;
 
 fn main() {
+    let src = "(+ (10 20))";
+    let mut l = Lexer::new(src);
+    println!("{:?}", l.lex().unwrap());
 }
