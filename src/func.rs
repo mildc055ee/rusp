9ea774b6b027c92bd3c97ab5b8becf3bbecf094b
mildc055ee <uncxochtil@gmail.com>
2020-04-10 18:53:28 +0900

    add some test for funcs.rs

--- src/func.rs
+++ src/func.rs
@@ -50,3 +50,46 @@ pub fn cdr(s: SExpr) -> SExpr {
         _ => atom!(Symbol::Nil)
     }
 }
+
+
+#[test]
+fn for_atom(){
+    let a = atom!(Symbol::Nil);
+    let b = SExpr::List{car: Box::new(atom!(Symbol::T)), cdr: Box::new(atom!(Symbol::Nil))};
+
+    assert_eq!(atom!(Symbol::T), atom(a));
+    assert_eq!(atom!(Symbol::F), atom(b));
+}
+
+#[test]
+fn for_cons(){
+    let a = atom!(Symbol::T);
+    let b = atom!(Symbol::T);
+    let expect = SExpr::List{car: Box::new(atom!(Symbol::T)), cdr: Box::new(atom!(Symbol::T))};
+
+    assert_eq!(expect, cons(a, b));
+}
+
+#[test]
+fn for_eq(){
+    let a1 = atom!(Symbol::Int(10));
+    let a2 = atom!(Symbol::Int(10));
+    assert_eq!(atom!(Symbol::T), eq(a1, a2));
+
+    let l1 = cons(atom!(Symbol::Nil), atom!(Symbol::T));
+    let l2 = cons(atom!(Symbol::Nil), atom!(Symbol::T));
+    assert_eq!(atom!(Symbol::T), eq(l1, l2));
+}
+
+#[test]
+fn for_car(){
+    let l1 = cons(atom!(Symbol::Int(32)), atom!(Symbol::Int(50)));
+    assert_eq!(atom!(Symbol::Int(32)), car(l1));
+}
+
+#[test]
+fn for_cdr(){
+    let l1 = cons(atom!(Symbol::Int(32)), atom!(Symbol::Int(50)));
+    assert_eq!(atom!(Symbol::Int(50)), cdr(l1));
+}
+
